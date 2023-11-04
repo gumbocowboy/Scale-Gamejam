00ad51f26c04c547438be6c737ec5f1613a50e8d
adam <meltymiko@gmail.com>
2023-11-03 22:19:03 -0400

    Cleaned up Scaling from potions, added Potionbase.

--- Assets/Scripts/ObjectScaler.cs
+++ Assets/Scripts/ObjectScaler.cs
@@ -6,50 +6,41 @@ public class ObjectScaler : MonoBehaviour
 {
     public GameObject objToScale;
 
-    public int objSize;
-    [Tooltip("The in-game size of the object that will effect puzzles")]
-    public float sizeChange;
-    [Tooltip("Will this item scale the player up")]
-    public bool scaleUp = false;
     
-    public Vector3 objMaxSize = new Vector3(10.0f, 10.0f, 10.0f);
-    public Vector3 objMinSize;
-    public Vector3 changeVector;
+    
+    
+    
 
-
+    Vector3 changeVector;
 
     Vector3 currentSize;
-    Vector3 targetSize;
-  
+    Vector3 targetSize;  
     Vector3 sizeDiff;
 
     float smoothSpeed = 0.3f;
     void Start()
     {
-        sizeDiff = new Vector3(sizeChange, sizeChange, sizeChange);
-        Debug.Log(this.gameObject.name + " sizechange is " + sizeChange);
+/*        sizeDiff = new Vector3(sizeChange, sizeChange, sizeChange);
+        Debug.Log(this.gameObject.name + " sizechange is " + sizeChange);*/
     }
 
     void Update()
     {
-        changeVector = sizeDiff;
     }
 
 
-    public void StartScaling()
+    public void StartScaling(int scaleAmt, bool scaleUp)
     {
-        Debug.Log("Scaling called from " + this.gameObject.name);
-        Debug.Log(this.gameObject.name + " sizechange is " + sizeChange);
+
+        changeVector = new Vector3(scaleAmt,scaleAmt,scaleAmt);
 
         if (scaleUp)
         {
             StartCoroutine(ScaleUp(objToScale, changeVector, 5f));
-
         }
         else
         {
             StartCoroutine(ScaleDown(objToScale, changeVector, 5f));
-
         }
 
     }
@@ -59,39 +50,27 @@ public class ObjectScaler : MonoBehaviour
 
     public IEnumerator ScaleUp(GameObject obj, Vector3 scaleTo, float seconds)
     {
-        float elapsedTime = 0;
+
         Vector3 startScale = objToScale.transform.localScale;
         Vector3 targetSize = startScale += scaleTo;
-        /*        while (elapsedTime < seconds)
-                {
-                    objToScale.transform.localScale = Vector3.Lerp(startScale, targetSize, (elapsedTime / seconds));
-                    elapsedTime += Time.deltaTime;
-                    yield return new WaitForEndOfFrame();
-
-                }*/
+ 
         yield return new WaitForEndOfFrame();
 
         objToScale.transform.localScale = targetSize;
-        GlobalVars.currentHeight += (int)sizeChange;
+        GlobalVars.currentHeight += (int)scaleTo.x;
 
     }
 
     public IEnumerator ScaleDown(GameObject obj, Vector3 scaleTo, float seconds)
     {
-        float elapsedTime = 0;
+
         Vector3 startScale = objToScale.transform.localScale;
         Vector3 targetSize = startScale -= scaleTo;
-        /*  while (elapsedTime < seconds)
-          {
-              objToScale.transform.localScale = Vector3.Lerp(startScale, targetSize, (elapsedTime / seconds));
-              elapsedTime += Time.deltaTime;
 
-              yield return new WaitForEndOfFrame();
-          }*/
         yield return new WaitForEndOfFrame();
 
         objToScale.transform.localScale = targetSize;
-        GlobalVars.currentHeight -= (int)sizeChange;
+        GlobalVars.currentHeight -= (int)scaleTo.x;
     }
 
 }
--- Assets/Scripts/PotionBase.cs
+++ Assets/Scripts/PotionBase.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// The base script to be attached to all Potions
+/// </summary>
 public class PotionBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int heightAmt;
+    public string mouseoverText;
+    public UIController uiCont;
+    public bool scaleUp = false;
+
+    void OnMouseOver()
     {
-        
+        uiCont.ShowMouseOver(mouseoverText);    
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseExit()
     {
-        
+        uiCont.HideMouseOver();
+    }
+
+    void OnMouseDown()
+    {
+        ObjectScaler scaler = this.GetComponent<ObjectScaler>();
+        scaler.StartScaling(heightAmt, scaleUp);
     }
 }
