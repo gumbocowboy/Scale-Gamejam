94dabe3e5be7e260f2e07d53c456b4c7ef718185
adam <meltymiko@gmail.com>
2023-11-12 11:26:20 -0500

    Added script to check range from mousePosition.

---
+++ Assets/Scripts/RangeFinder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+
+public class RangeFinder : MonoBehaviour
+{
+    public float maxDistance = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        if(Physics.Raycast(ray, out hit, maxDistance))
+        {
+            if(hit.collider.gameObject.layer == 3)
+            {
+                if(hit.collider.gameObject.tag == "Potions")
+                {
+                    Debug.Log("Potion");
+                }
+
+                if (hit.collider.gameObject.tag == "Moveables")
+                {
+                    Debug.Log("Moveab");
+                }
+
+            }
+        }
+    }
+}
